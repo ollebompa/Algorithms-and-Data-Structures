c423e8240087a47a65cd5de37c02b974808f3d84
ollenilsson19 <oen19@ic.ac.uk>
2020-01-10 12:51:42 +0000

    added selection sort solution

--- selection_sort.py
+++ selection_sort.py
@@ -7,7 +7,7 @@ def selection_sort(array):
     for i in range(len(array)):
         min_index = i
         for j in range(i+1, len(array)):
-            if array[j] < array[min_indexection]:
+            if array[j] < array[min_index]:
                 min_index = j
         array[i], array[min_index] = array[min_index], array[i]
 
